a0c110522bfb0f43adc2317b6c7aead3511ccaa1
Tushar Batra <tusharbatra08@gmail.com>
2024-04-26 23:03:51 +0530

    Changes in 3rd Question.

--- Q3.js
+++ Q3.js
@@ -1,13 +1,15 @@
 //  Factorial of a number
 
 function factorial(num) {
-    let ans = 1;
+  let ans = 1;
 
-    if (num === 0)
-        console.log(1)
-    for (let i = 1; i <= num; i++)
-        ans = ans * i
-    console.log(ans);
+  if (num === 0) {
+    console.log(1);
+  }
+  for (let i = 1; i <= num; i++) {
+    ans = ans * i;
+  }
+  console.log(ans);
 }
 
-factorial(4)
+factorial(4);
